987fcebb35e0fca12f9ebbbe516609203bd84e3c
RoshanC4 <149266230+RoshanC4@users.noreply.github.com>
2025-01-23 19:05:50 -0600

    Updated clickable images

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -61,15 +61,15 @@ var config_data = `
             "min": 1,
             "max": 99999
         },
-        {
-            "name": "Auto Start Position",
-            "code": "as",
-            "type": "clickable_image",
-            "filename": "2025/field_image.png",
-            "clickRestriction": "one",
-            "allowableResponses": "6 7 18 19 30 31 42 43 54 55 66 67",
-            "shape": "circle 5 black red true"
-        }
+        { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2025/half_field.png",
+      "clickRestriction": "one",
+      "dimensions": "6 6",
+      "allowableResponses": "1 7 13 19 25 31",
+      "shape": "circle 5 black red true"
+    }
     ],
     "auton": [
         {
@@ -77,14 +77,14 @@ var config_data = `
             "code": "al",
             "type": "bool"
         },
-        {
-            "name": "Auto Scoring Locations",
-            "code": "asl",
-            "type": "clickable_image",
-            "filename": "2025/field_image.png",
-            "allowableResponses": "3 4 15 16 27 28 39 40 52 53 64 65",
-            "shape": "circle 5 black red true"
-        },
+         { "name": "Auto Scoring Position",
+      "code": "asp",
+      "type": "clickable_image",
+      "filename": "2025/reef.png",
+      "dimensions": "6 6",
+      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
+      "shape": "circle 5 black red true"
+    },
         {
             "name": "Coral L1",
             "code": "ac1",
