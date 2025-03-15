d03422b4506fd1d0a7ae9f0f431fb562e3f9198d
RoshanC4 <149266230+RoshanC4@users.noreply.github.com>
2025-03-15 11:46:52 -0500

    increased comment box character limit

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -247,7 +247,7 @@ var config_data = `
             "code": "co",
             "type": "text",
             "size": 15,
-            "maxSize": 100
+            "maxSize": 250
         }
     ]
 }`;
